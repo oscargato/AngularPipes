af9ffc9719eadcfe6d9eca3a5b9942888e6f3aa5
Oscar Mejia <oscardongato@gmail.com>
2019-01-16 08:44:40 -0400

    Commit Base de Angular Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre = "Fernando";
+  nombre2 = "osCar joSe meJIa ferNAdez";
+  arreglo = [1,2,3,4,5,6,7,8,9,0];
+  Pi = Math.PI;
+  a = 0.234;
+  salario = 1234.5
+
+  heroe = {
+  	nombre: "Logan",
+  	clave: "Wolverine",
+  	edad: 500,
+  	direccion:{
+  		calle: "Primera",
+  		casa: "19"
+  	}
+  }
+
+  valorDePromesa = new Promise( (resolve,reject)=>{
+  	setTimeout(()=>resolve('Llego la Data!!!'),3500);
+  });
+
+  fecha = new Date();
+
+  video = "https://www.youtube.com/watch?v=eh8eb_ACLl8&start_radio=1&list=RDeh8eb_ACLl8";
+
+  activar:boolean = true;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
+import { LOCALE_ID,NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
+import localeES from '@angular/common/locales/es';
+import { registerLocaleData } from '@angular/common';
+import { CapitalizadoPipe } from './pipes/capitalizado.pipe';
+import { DomseguroPipe } from './pipes/domseguro.pipe';
+import { ContrasenaPipe } from './pipes/contrasena.pipe';
+import { FooterComponent } from './components/footer/footer.component';
+
+registerLocaleData(localeES);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CapitalizadoPipe,
+    DomseguroPipe,
+    ContrasenaPipe,
+    FooterComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+  	{provide: LOCALE_ID, useValue:'es' }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
